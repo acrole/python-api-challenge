6dd48b8a15e3bf2290747d0712165a17f9b4cc78
acrole <averycroley@gmail.com>
2020-12-15 14:17:46 -0600

    installed and fixed the citipy

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "4b639db4b5f14e8cbfed6409e5a882df"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyA7UzrgOLySf6BCdEY0X9wuaH94l1nQUY0"
